78983c31be8554e148fb65e35be5e03043a65991
shunyk <48780737+shunyk@users.noreply.github.com>
2019-06-23 18:00:28 +0900

    add

--- WebContent/js/found.js
+++ WebContent/js/found.js
@@ -2,39 +2,21 @@ $(function() {
 	
 	var location="";
 	var pro = "";
-	$('#ll').change(function() {
-		var xhttp;
-		if(window.XMLHttpRequest){
-			xhttp = new XMLHttpRequest();
-		}else{
-			xhttp= new ActiveXObject("Microsoft.XMLHTTP");	
-		}
-		
+	$('.l').click(function() {
 		location = $("#ll").text();
 		pro=$("#pp").text();
 		
-		xhttp.open("post", "./ExpertFound", true);
-		xhttp.setRequestHeader("location", location, "pro" , pro);
-		
-		xhttp.send();
-		
-		xhttp.onreadystatechange=function(){
-			if(this.readyState==4 && this.status==200){
-				$("#expert").html('<c:if test="${list != null}">'+
-				'<c:forEach begin="0" step="1" end="${list.size() }" var="i">'+
-					'<div>포트폴리오 준비중</div>'+
-					'<div id="expert">'+
-					'<div id="expert_info">'+
-							'<img alt="profile" src=""> <span id="name">${list[i].getName()}</span>'+
-							'<span>상담신청</span>'+
-						'</div>'+
-						'<div id="info">'+
-							'<p>${list[i].getInfo()}</p>'+
-						'</div>'+
-					'</div>'+
-				'</c:forEach>'+
-			'</c:if>');
-			}
+		if(pro=="필요시공"){
+			pro="";
 		}
+		$("#location").val(location);
+		$("#pro").val(pro);
+	});
+	
+	$('.p').click(function() {
+		location = $("#ll").text();
+		pro=$("#pp").text();
+		$("#location").val(location);
+		$("#pro").val(pro);
 	});
 });
